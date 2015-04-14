1ec1bab9bf189a102e801c5aa4c4412edded877a
Cornelius Bock <zweihundertdrei@gmail.com>
2015-04-14 16:43:23 +0200

    change paramstyle to 'qmark'

--- pyhdb/__init__.py
+++ pyhdb/__init__.py
@@ -24,7 +24,7 @@ from pyhdb.exceptions import *
 
 apilevel = "2.0"
 threadsafety = 2
-paramstyle = ("qmark", "named", "format")
+paramstyle = "qmark"
 
 
 def connect(host, port, user, password, autocommit=False):
