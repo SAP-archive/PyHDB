8efa7ee348895ebfe242f84c4059f07b7db6c553
Timo Djürken <timo.djuerken@gmail.com>
2017-05-05 14:48:37 +0200

    Add reconnect option

--- pyhdb/__init__.py
+++ pyhdb/__init__.py
@@ -25,8 +25,8 @@ paramstyle = "numeric"
 tracing = os.environ.get('HDB_TRACE', 'FALSE').upper() in ('TRUE', '1')
 
 
-def connect(host, port, user, password, autocommit=False):
-    conn = Connection(host, port, user, password, autocommit)
+def connect(host, port, user, password, autocommit=False, reconnect=True):
+    conn = Connection(host, port, user, password, autocommit, reconnect)
     conn.connect()
     return conn
 
